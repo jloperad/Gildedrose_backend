ac189c903dada586470dcd7c88c0e80dd2173ba7
fsalazarr <55804707+fsalazarr@users.noreply.github.com>
2022-04-04 22:17:51 -0500

    Homework 3 - First commit

---
+++ Homework_3/maven.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64
+export M2_HOME=/opt/apache-maven-3.8.5
+export MAVEN_HOME=/opt/apache-maven-3.8.5
+export PATH=${M2_HOME}/bin:${PATH}
---
+++ Homework_3/startapp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar /home/vagrant/application/target/gildedrose-0.0.1-SNAPSHOT.jar
