b60288c04349e718459d6c0f1d96984f6dd382fb
fsalazarr <55804707+fsalazarr@users.noreply.github.com>
2022-04-05 23:35:58 -0500

    Homework 3 with bonus 2 done.

--- Homework_3/prerequisites.sh
+++ Homework_3/prerequisites.sh
@@ -1,42 +1,29 @@
 #!/usr/bin/env bash
 
-"Usefull links
-https://www.digitalocean.com/community/tutorials/como-instalar-y-usar-docker-en-ubuntu-18-04-1-es
-https://linuxize.com/post/how-to-install-apache-maven-on-ubuntu-18-04/
-https://www.digitalocean.com/community/tutorials/como-instalar-java-con-apt-en-ubuntu-18-04-es
-"
 
 apt-get update
-# Git installation
-apt-get install -y git-all
-# Java installation
-apt-get install -y openjdk-17-jdk
-# Maven installation
-wget https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz -P /tmp
-tar xf /tmp/apache-maven-*.tar.gz -C /opt
+echo  "-- Git installation --" # Default version, outdated but works.
+apt-get install -y git
+echo  "-- Java installation --"
+apt-get install -y openjdk-17-jdk # Java 17 Version, required.
+echo  "-- Maven installation --"
+wget https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz -P /tmp 
+tar xf /tmp/apache-maven-*.tar.gz -C /opt # Latest version at the moment, required.
 cp /vagrant/maven.sh /etc/profile.d/
-source /etc/profile.d/maven.sh
-# Docker installation
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
-apt-cache policy docker-ce
-apt-get install -y docker-ce
-# Fetch and run postgres database in docker
+source /etc/profile.d/maven.sh 
+echo  "-- Docker installation --" 
+apt-get install -y docker.io # Default version, outdated but works.
+echo  "-- Fetch and run postgres database in docker --"
 docker run --name my-postgres -e POSTGRES_PASSWORD=secret -p 5433:5432 -d postgres
-# Git clone the application
+echo  "-- Git clone the application --"
 mkdir /home/vagrant/application
 git clone https://github.com/Eagleciam/praxis-gildedrose.git /home/vagrant/application
 cd /home/vagrant/application
 mvn install
-# Launch the app
+echo  "-- Launching the app --"
 java -jar /home/vagrant/application/target/gildedrose-0.0.1-SNAPSHOT.jar &
 # Bonus 2 : Add app start as init service
+cp /vagrant/startapp.sh /home/vagrant/
 cp /vagrant/appboot.service /etc/systemd/system/
-cp /vagrant/startdocker.sh /etc/init.d/
-cp /vagrant/startapp.sh /etc/init.d/
-update-rc.d startdocker.sh defaults 98 02
-update-rc.d startapp.sh defaults 98 02
 systemctl enable appboot.service
 echo "Done"
--- Homework_3/startapp.sh
+++ Homework_3/startapp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+sudo docker start my-postgres
 java -jar /home/vagrant/application/target/gildedrose-0.0.1-SNAPSHOT.jar
